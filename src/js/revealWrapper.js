38cbb99f7d2ff784a8bee86e7810e74b33e99600
Bartlomiej Pietrowski <bartlomiej.pietrowski@capgemini.com>
2016-10-05 11:02:49 +0200

    Added code highlighting (solarized-light theme), added hljs initialization, changed transition to cube and removed reveal default controls.

--- src/js/revealWrapper.js
+++ src/js/revealWrapper.js
@@ -1,20 +1,22 @@
 import Reveal from 'reveal';
 import classList from '/jspm_packages/github/hakimel/reveal.js@3.3.0/lib/js/classList.js';
+import hljs from '/jspm_packages/github/hakimel/reveal.js@3.3.0/plugin/highlight/highlight.js';
 import zoom from '/jspm_packages/github/hakimel/reveal.js@3.3.0/plugin/zoom-js/zoom.js';
 
 Reveal.initialize({
-    controls: true,
+    controls: false,
     progress: true,
     history: true,
     center: true,
     overview: true,
     touch: true,
     theme: window.Reveal.getQueryHash().theme, // available themes are in /css/theme
-    transition: 'linear', // default/cube/page/concave/zoom/linear/fade/none
+    transition: 'cube', // default/cube/page/concave/zoom/linear/fade/none
     dependencies: [
         // do not use it - use system JS instead.
     ]
 });
 
+hljs.initHighlighting();
 
 export default Reveal;
